8abd867eef5fa1dc75cec14f896c669d032b5b08
Ewan Z <ewancoder@gmail.com>
2018-02-07 23:13:36 +0300

    Add Task and Project tests for Entity subject

---
+++ Lifestyle.Planning.Domain.Tests/ProjectEntityTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Lifestyle.Planning.Domain.Tests
+{
+    using Lifestyle.Shared.Tests;
+    using Xunit;
+
+    [Trait("Category", "Project entity")]
+    public sealed class ProjectEntityTests : EntityTests<Project, ProjectId>
+    {
+        private readonly ProjectId _sameId = ProjectId.New();
+        private readonly ProjectId _anotherId = ProjectId.New();
+
+        protected override ProjectId SameIdentity() => _sameId;
+        protected override ProjectId AnotherIdentity() => _anotherId;
+        protected override Project CreateEntity(ProjectId identity)
+            => new Project(new Project.State { ProjectId = identity });
+    }
+}
