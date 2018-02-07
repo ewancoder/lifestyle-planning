dd7e85579519ff8b2a53a0b99b0f4a365d52a050
Ewan Z <ewancoder@gmail.com>
2018-02-07 20:31:23 +0300

    Create shared tests project and update dependencies

---
+++ Lifestyle.Shared.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lifestyle.Shared.Tests")]
+[assembly: AssemblyDescription("Lifestyle Shared Tests")]
+[assembly: AssemblyCompany("Lifestylej")]
+[assembly: AssemblyProduct("Lifestyle.Shared.Tests")]
+[assembly: AssemblyCopyright("Copyright © Lifestyle 2018")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
